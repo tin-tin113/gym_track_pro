7a4e3ff094d403cfd8fcdd9aaffe26930ad3cddd
Martin Diasanta <martincantordiasanta@gmail.com>
2026-06-12 17:13:46 +0800

    feat: add automated demo data seeding to build script

--- build.sh
+++ build.sh
@@ -10,3 +10,7 @@ python django_app/manage.py migrate
 
 # Collect static files
 python django_app/manage.py collectstatic --no-input
+
+# Seed demo data (needed for Render Free Tier since interactive shell is disabled)
+python django_app/manage.py seed_demo
+
