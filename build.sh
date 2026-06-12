78d89dda561a8a891cd39a397836d049769985e0
Martin Diasanta <martincantordiasanta@gmail.com>
2026-06-12 17:29:19 +0800

    feat: configure project URLs for media serving and update build script to include image deployment and additional seed tasks

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Ensure media directory exists and copy guide images from tracked static files
+mkdir -p django_app/media/guide_images
+cp django_app/tracker/static/images/*.jpg django_app/media/guide_images/
+
+
 # Run migrations
 python django_app/manage.py migrate
 
@@ -13,4 +18,7 @@ python django_app/manage.py collectstatic --no-input
 
 # Seed demo data (needed for Render Free Tier since interactive shell is disabled)
 python django_app/manage.py seed_demo
+python django_app/manage.py seed_approved_guides
+python django_app/manage.py seed_guides
+
 
--- django_app/gymtrack_django/urls.py
+++ django_app/gymtrack_django/urls.py
@@ -1,13 +1,10 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
     path('', include('tracker.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
